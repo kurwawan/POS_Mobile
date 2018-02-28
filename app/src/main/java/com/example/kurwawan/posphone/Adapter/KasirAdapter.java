af7b09c2c05de3467377277e1e40606d6c90f923
kurwawan <kurniawanridwansurohardjo@gmail.com>
2018-02-28 13:25:29 +0700

    rev

---
+++ app/src/main/java/com/example/kurwawan/posphone/Adapter/KasirAdapter.java
@@ -0,0 +1,8 @@
+package com.example.kurwawan.posphone.Adapter;
+
+/**
+ * Created by zerotohero on 26/02/18.
+ */
+
+public class KasirAdapter {
+}
---
+++ app/src/main/java/com/example/kurwawan/posphone/Model/Kasir.java
@@ -0,0 +1,8 @@
+package com.example.kurwawan.posphone.Model;
+
+/**
+ * Created by zerotohero on 28/02/18.
+ */
+
+public class Kasir {
+}
