7e6a5b89dc27701cb79137c925110135723f3933
kurwawan <kurniawanridwansurohardjo@gmail.com>
2018-03-02 23:35:03 +0700

    updated

---
+++ app/src/main/java/com/example/kurwawan/posphone/Adapter/ProdukAdapter.java
@@ -0,0 +1,8 @@
+package com.example.kurwawan.posphone.Adapter;
+
+/**
+ * Created by zerotohero on 02/03/18.
+ */
+
+public class ProdukAdapter {
+}
---
+++ app/src/main/java/com/example/kurwawan/posphone/Model/Produk.java
@@ -0,0 +1,8 @@
+package com.example.kurwawan.posphone.Model;
+
+/**
+ * Created by zerotohero on 02/03/18.
+ */
+
+public class Produk {
+}
